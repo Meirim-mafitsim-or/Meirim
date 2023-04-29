750a06e1a814f36521b6d0427ce1a1b6e4379fb5
shoval15 <shoval1509@gmail.com>
2023-04-29 16:23:53 +0300

    update login.js file

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,10 +4,40 @@ import Card from 'react-bootstrap/Card';
 import strings from '../static/Strings.json';
 import { useContext } from 'react';
 import { LanguageContext } from '../common/LanguageContext';
+import { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 
 function Login() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBC742Q2W8Z5AM_5GcMEuUZLjhUQxw4aU",
+    authDomain: "meirim-b3c4f.firebaseapp.com",
+    projectId: "meirim-b3c4f",
+    storageBucket: "meirim-b3c4f.appspot.com",
+    messagingSenderId: "725325643196",
+    appId: "1:725325643196:web:7d38a099023dca9d35bfb9",
+    measurementId: "G-YRT0FDPHJC"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+
   const { language } = useContext(LanguageContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <Card className="text-center w-50 m-auto mt-5">
     <Card.Body>
