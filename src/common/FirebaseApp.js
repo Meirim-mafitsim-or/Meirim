80a51a6002c2895feda5a7ac053e1fe5ed0f2e75
shoval15 <shoval1509@gmail.com>
2023-04-29 18:06:31 +0300

    update login.js file - checked success login

--- src/common/FirebaseApp.js
+++ src/common/FirebaseApp.js
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from 'firebase/auth';
-
 import "firebase/auth";
 
 
@@ -23,9 +21,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = getAuth(app);
 export default app;
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,8 +5,8 @@ import strings from '../static/Strings.json';
 import { useContext } from 'react';
 import { LanguageContext } from '../common/LanguageContext';
 import { useState } from "react";
-import {auth} from "../common/FirebaseApp";
-import { useHistory } from "react-router-dom";
+import {app} from '../common/FirebaseApp';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 
 
@@ -14,10 +14,13 @@ function Login() {
   const { language } = useContext(LanguageContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const auth = getAuth(app);
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      await auth.signInWithEmailAndPassword(email, password);
+      await signInWithEmailAndPassword(auth, email, password);
       console.log("success");
 
     } catch (error) {
@@ -27,20 +30,29 @@ function Login() {
   return (
     <Card className="text-center w-50 m-auto mt-5">
     <Card.Body>
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>{strings.email[language]}</Form.Label>
-        <Form.Control type="email" placeholder={strings.email_placeholder[language]} />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>{strings.login_password[language]}</Form.Label>
-        <Form.Control type="password" placeholder={strings.login_password[language]} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        {strings.login[language]}
-      </Button>
-    </Form>
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>{strings.email[language]}</Form.Label>
+      <Form.Control
+        type="email"
+        placeholder={strings.email_placeholder[language]}
+        value={email} // add value attribute here
+        onChange={(e) => setEmail(e.target.value)} // add onChange event handler here
+      />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>{strings.login_password[language]}</Form.Label>
+      <Form.Control
+        type="password"
+        placeholder={strings.login_password[language]}
+        value={password} // add value attribute here
+        onChange={(e) => setPassword(e.target.value)} // add onChange event handler here
+      />
+    </Form.Group>
+    <Button variant="primary" type="submit" onClick={handleLogin}>
+      {strings.login[language]}
+    </Button>
+  </Form>
     </Card.Body>
   </Card>
 
