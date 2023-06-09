ff0cf10f6093cdc5fa7d1e9d26d09d966eefc9f7
Shoval15 <127046404+Shoval15@users.noreply.github.com>
2023-06-09 16:20:31 +0300

    Shoval dev (#23)
    * add assigning option, edit families file
    
    * without family id
    
    * with family id
    
    * while cancle the family's  confirm  - cancle the assigning of this family
    
    * when assigning, write to collection assignings
    
    * filter by history assigning
    
    * remove unused varaible
    
    * remove unused varaible
    
    * remove unused varaible
    
    * Adjustment to the change in firebase - campers will be displayed as a map of ID, family, assigning
    
    * Adjustment to the change in firebase - campers will be displayed as a map of ID, family, assigning
    
    * Adjustment to the change in firebase

---
+++ src/common/Assigning.js
@@ -0,0 +1,342 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import strings from '../static/Strings.json';
+import { BiUserPlus } from 'react-icons/bi';
+import { db } from '../common/FirebaseApp';
+import { updateDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { setDoc } from 'firebase/firestore';
+import { BiCheck } from 'react-icons/bi';
+
+export async function getCampers(event, setRegID) {
+  const EventsSnapshot = await getDoc(event);
+  const eventData = EventsSnapshot.data();
+  setRegID(eventData.registrationId);
+  const campersID  = eventData.campers;
+  const campersSnapshot = await getDocs(collection(db, 'campers'));
+
+  const campersData = [];
+  
+  campersSnapshot.forEach((camperDoc) => {
+    const camperData = camperDoc.data();
+    const camperId = camperDoc.id;
+    const camperDataWithId = { ...camperData, id: camperId };
+    campersData.push(camperDataWithId);
+  });
+
+  const joinedCampers = campersID.map((camperID) => {
+    const foundCamper = campersData.find((camper) => camper.id === camperID.id);
+    return {
+      ...camperID,
+      ...foundCamper
+    };
+  });
+  return joinedCampers;
+
+}
+
+export default function Assigning({
+  show,
+  onHide,
+  language,
+  event,
+  selectedFamily,
+  setFamilies,
+  settlement,
+}) {
+  const [campers, setCampers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortBy, setSortBy] = useState('age');
+  const [filterByGender, setFilterByGender] = useState('');
+  const [regID, setRegID] = useState(null);
+  const [historyAss, setHistoryAss] = useState([]);
+  const [showFilteredCampers, setShowFilteredCampers] = useState(false);
+
+  useEffect(() => {
+    const fetchCampers = async () => {
+      try {
+        const fetchedCampers = await getCampers(event, setRegID);
+        setCampers(fetchedCampers);
+        if (show && selectedFamily) {
+          await checkCamperIDInAssigningsArray(
+            selectedFamily.last_name,
+            selectedFamily.phone_number
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCampers(); // Call the fetchCampers function
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [event, selectedFamily]); // Include selectedFamily as a dependency
+
+  // Filter campers based on search query and gender filter
+  const filteredCampers = campers.filter(
+    (camper) =>
+      (camper.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        camper.lastName.toLowerCase().includes(searchQuery.toLowerCase())) &&
+      (!filterByGender || camper.gender === filterByGender) &&
+      (!showFilteredCampers || historyAss.includes(camper.id))
+  );
+
+  // Sort campers based on the selected sorting option
+  const sortedCampers = filteredCampers.sort((a, b) => {
+    if (sortBy === 'age') {
+      return a.age - b.age;
+    }
+    return 0;
+  });
+
+  // Fetch the assignings document for the selected settlement
+  const getFamilyFromAssignings = async (settlement) => {
+    if (!selectedFamily) {
+      console.log('No selected family.');
+      return null;
+    }
+
+    const collectionRef = collection(db, 'assignings');
+    const docRef = doc(collectionRef, settlement);
+    const docSnapshot = await getDoc(docRef);
+
+    if (!docSnapshot.exists()) {
+      console.log('Assigning document not found.');
+      return null;
+    }
+
+    const assigningsData = docSnapshot.data();
+    const assigningsArray = assigningsData.assignings || []; // Initialize with an empty array
+
+    let assigningExists = null;
+
+    assigningsArray.some((assigning) => {
+      if (
+        assigning.familyName === selectedFamily.last_name &&
+        assigning.phoneNumber === selectedFamily.phone_number
+      ) {
+        // Object matching the conditions found in the array
+        assigningExists = assigning;
+        return true; // Exit the loop
+      }
+      return false;
+    });
+
+    return { assigningExists, assigningsArray };
+  };
+
+  // Add a family to the assignings document
+  const addFamilyToAssignings = async (camperID) => {
+    if (!show) {
+      return;
+    }
+
+    const { assigning, assigningsArray } = await getFamilyFromAssignings(settlement);
+    const collectionRef = collection(db, 'assignings');
+    const docRef = doc(collectionRef, settlement);
+    if (!assigning) {
+      // Create a new object with the selected family details
+      const newAssigning = {
+        familyName: selectedFamily.last_name,
+        phoneNumber: selectedFamily.phone_number,
+        campersId: [camperID],
+      };
+
+      // Add the new object to the assigningsArray
+      assigningsArray.push(newAssigning);
+
+      // Update the Firestore document
+      await updateDoc(docRef, { assignings: assigningsArray });
+
+      console.log('New object added to assigningsArray:', newAssigning);
+    } else {
+      if (!assigning.campersId.includes(camperID)) {
+        assigning.campersId.push(camperID);
+        await updateDoc(docRef, { assignings: assigningsArray });
+      }
+    }
+  };
+
+  // Check if the camper ID exists in the assignings array for the selected family
+  const checkCamperIDInAssigningsArray = async (last_name, phone_number) => {
+    const result = await getFamilyFromAssignings(settlement);
+
+    if (result !== null) {
+      // eslint-disable-next-line no-unused-vars
+      const { assigningExists: ignored, assigningsArray } = await getFamilyFromAssignings(settlement);
+      
+      if (!assigningsArray || assigningsArray.length === 0) {
+        return;
+      }
+
+      const foundAssigning = assigningsArray.find(
+        (assigning) => assigning.familyName === last_name && assigning.phoneNumber === phone_number
+      );
+
+      if (foundAssigning) {
+        await setHistoryAss(foundAssigning.campersId);
+        return foundAssigning.campersId;
+      }
+    } else 
+    {
+      return [];
+    }
+    
+
+    return [];
+  };
+
+  // Handle click event when the user clicks the "User Plus" icon to assign a camper to a family
+  const handleUserPlusClick = async (index) => {
+    if (show) {
+      addFamilyToAssignings(campers[index].id);
+    }
+
+    const updatedCampers = [...campers];
+    const familiesCol = collection(db, 'familiesRegistration');
+    const cur_families = doc(familiesCol, regID);
+
+    const FamiliesSnapshot = await getDoc(cur_families);
+    const familiesData = FamiliesSnapshot.data();
+
+    const families = familiesData.families;
+
+    if (updatedCampers[index].assigning) {
+      const confirmed = window.confirm(strings.to_change_assigning[language]);
+      if (!confirmed) {
+        return; // Don't proceed further if not confirmed
+      }
+      const lastFamily = families.find((check_family) => check_family.id === updatedCampers[index].family);
+      if (lastFamily) {
+        lastFamily.assigning = false;
+        lastFamily.camper = null;
+      }
+    }
+
+    updatedCampers[index].assigning = true;
+    updatedCampers[index].family = selectedFamily.id;
+
+    const desiredFamilyIndex = families.findIndex((check_family) => check_family.id === selectedFamily.id);
+    if (desiredFamilyIndex !== -1) {
+      families[desiredFamilyIndex].camper = updatedCampers[index].id;
+      families[desiredFamilyIndex].assigning = true;
+    }
+
+    // Update the family object in the Firebase Firestore database
+    await Promise.all([
+      updateDoc(event, { campers: updatedCampers }),
+      setDoc(cur_families, { families }),
+    ]);
+    onHide();
+    setFamilies(families);
+  };
+
+  return (
+    <Modal show={show} onHide={onHide} centered className="custom-modal">
+      <Modal.Header closeButton>
+        <Modal.Title>{strings.assigning[language]}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="d-flex justify-content-between mb-3">
+          <div>
+            <label htmlFor="searchInput" className="form-label">
+              {strings.search_by_name[language]}
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="searchInput"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          <div className="d-flex align-items-start">
+            <div className="me-3">
+              <label htmlFor="sortSelect" className="form-label">
+                {strings.sort_by_age[language]}
+              </label>
+              <select
+                className="form-select"
+                id="sortSelect"
+                value={sortBy}
+                onChange={(e) => setSortBy(e.target.value)}
+              >
+                <option value="age">{strings.ascending[language]}</option>
+                <option value="-age">{strings.descending[language]}</option>
+              </select>
+            </div>
+            <div className="me-3">
+              <label htmlFor="filterSelect" className="form-label">
+                {strings.filter_by_gender[language]}
+              </label>
+              <select
+                className="form-select"
+                id="filterSelect"
+                value={filterByGender}
+                onChange={(e) => setFilterByGender(e.target.value)}
+              >
+                <option value="">{strings.all[language]}</option>
+                <option value="male">{strings.male[language]}</option>
+                <option value="female">{strings.female[language]}</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="form-check mb-1">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="filterCheckbox"
+            checked={showFilteredCampers}
+            onChange={(e) => setShowFilteredCampers(e.target.checked)}
+          />
+          <label className="form-check-label" htmlFor="filterCheckbox">
+            {strings.show_only_campers_in_history[language]}
+          </label>
+        </div>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>{strings.id[language]}</th>
+              <th>{strings.first_name[language]}</th>
+              <th>{strings.last_name[language]}</th>
+              <th>{strings.gender[language]}</th>
+              <th>{strings.age[language]}</th>
+              <th>{strings.special_comment[language]}</th>
+              <th>{strings.assigned[language]}</th>
+              <th>{strings.choose[language]}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedCampers.map((item, index) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.gender}</td>
+                <td>{item.age}</td>
+                <td>{item.comments}</td>
+                <td>{item.assigning ? <BiCheck size={20} /> : null}</td>
+                <td>
+                  <BiUserPlus
+                    size={30}
+                    onClick={() => handleUserPlusClick(index)}
+                    style={{ cursor: 'pointer' }}
+                    color="#313B72"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          {strings.close[language]}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
--- src/pages/Families.js
+++ src/pages/Families.js
@@ -1,98 +1,214 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Container, Button,  } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import { LanguageContext } from '../common/LanguageContext';
 import { db } from '../common/FirebaseApp';
-import { collection, doc, getDoc} from "firebase/firestore"; 
-import { useLocation } from 'react-router-dom';
-import strings from '../static/Strings.json';
+import { collection, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { BiPencil } from 'react-icons/bi';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsCheckCircleFill } from 'react-icons/bs';
+import { FaChild } from 'react-icons/fa';
+import Assigning from '../common/Assigning';
+import strings from '../static/Strings.json';
 
 
-export async function getFamilies(id) {
-    const events = collection(db, 'events');
-    const cur_event = doc(events, id);
-    const EventsSnapshot = await getDoc(cur_event);
-    const eventData = EventsSnapshot.data();
-    return eventData.families || [];
+// Function to retrieve families data from Firestore
+async function getFamilies(id, setFamiliesEvent, setFamiliesDoc, setEvent, setSettlement) {
+  const events = collection(db, 'events');
+  const cur_event = doc(events, id);
+  setEvent(cur_event);
+  const EventsSnapshot = await getDoc(cur_event);
+  const eventData = EventsSnapshot.data();
+  const families_id = eventData.registrationId;
+  setSettlement(eventData.settlement)
+  setFamiliesEvent(families_id);
+
+  if (families_id) {
+    const familiesRegCol = collection(db, 'familiesRegistration');
+    const familiesReg = doc(familiesRegCol, families_id);
+    setFamiliesDoc(familiesReg);
+    const familySnapshot = await getDoc(familiesReg);
+    if (familySnapshot.exists()) {
+      const familyData = familySnapshot.data();
+      return familyData.families;
+    } else {
+      console.log('Referenced family document does not exist.');
+      return null;
+    }
+  } else {
+    console.log('No reference to family document.');
+    return null;
   }
-
+}
 
 export default function Families() {
-    const { language } = React.useContext(LanguageContext);
-    const [families, setFamilies] = useState([]);
-    const location = useLocation();
-    const [pathSuffix, setPathSuffix] = useState('');
+  const { language } = React.useContext(LanguageContext);
+  const [families, setFamilies] = useState([]);
+  const [event, setEvent] = useState(null);
+  const location = useLocation();
+  const [pathSuffix, setPathSuffix] = useState('');
+  const [familiesEvent, setFamiliesEvent] = useState(null);
+  const [familiesDoc, setFamiliesDoc] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedFamily, setSelectedFamily] = useState(null);
+  const [settlement, setSettlement] = useState(null);
+  const navigate = useNavigate();
 
+  // Extract the suffix from the current URL path
+  useEffect(() => {
+    const path = location.pathname;
+    const parts = path.split('/');
+    const suffix = parts[parts.length - 1];
+    setPathSuffix(suffix);
+  }, [location]);
 
-    useEffect(() => {
-        const path = location.pathname;
-        const parts = path.split('/');
-        const suffix = parts[parts.length-1];
-        setPathSuffix(suffix);
-    }, [location]);
-    
-    useEffect(() => {
-        if (pathSuffix) {
-          getFamilies(pathSuffix)
-            .then((families) => setFamilies(families))
-            .catch((error) => console.log(error));
-        }
-      }, [pathSuffix]);
+  // Fetch families data based on the extracted suffix
+  useEffect(() => {
+    const fetchData = async () => {
+      const familiesData = await getFamilies(
+        pathSuffix, setFamiliesEvent, setFamiliesDoc, setEvent, setSettlement);
+      if (familiesData) {
+        setFamilies(familiesData);
+      }
+    };
+
+    fetchData().catch(error => console.log(error));
+  }, [pathSuffix]);
+
+  const editClick = async (family) => {
+    setSelectedFamily(family);
+    navigate(`./${familiesEvent}`, { state: { familiesId: familiesEvent, selectedFamily: family, event_id: pathSuffix } });
+  };
+
+  const confirm = async (index, bool) => {
+    const updatedFamilies = [...families];
+    const familyToUpdate = { ...updatedFamilies[index] };
+    const isAssigned = familyToUpdate.assigning;
+    familyToUpdate.confirmed = bool;
+    familyToUpdate.assigning = false;
+    const lastCamper = familyToUpdate.camper;
+    familyToUpdate.camper = null;
+    updatedFamilies[index] = familyToUpdate;
+  
+    // Update specific fields in familiesDoc
+    await updateDoc(familiesDoc, {
+      families: updatedFamilies.map((family) => ({
+        ...family,
+        confirmed: family.id === familyToUpdate.id ? bool : family.confirmed,
+        assigning: family.id === familyToUpdate.id ? false : family.assigning,
+        camper: family.id === familyToUpdate.id ? null : family.camper,
+      })),
+    });
+  
+    setFamilies(updatedFamilies);
+  
+    const eventsSnapshot = await getDoc(event);
+    const eventData = eventsSnapshot.data();
+    const confirmedFamilies = eventData.families;
+    const campers = [...eventData.campers];
+  
+    if (bool) {
+      const updatedConfirmedFamilies = [...confirmedFamilies, familyToUpdate.id];
+      await updateDoc(event, { families: updatedConfirmedFamilies });
+    } else {
+      if (isAssigned) {
+        const updatedCampers = campers.map((camper) => {
+          if (camper.id === lastCamper) {
+            return { ...camper, assigning: false, family: null };
+          }
+          return camper;
+        });
+        await updateDoc(event, {
+          families: confirmedFamilies.filter((family) => family !== familyToUpdate.id),
+          campers: updatedCampers,
+        });
+      } else {
+        await updateDoc(event, {
+          families: confirmedFamilies.filter((family) => family !== familyToUpdate.id),
+        });
+      }
+    }
+  };
+  
+
+  const handleAssigningModal = (family) => {
+    setSelectedFamily(family);
+    setShowModal(true);
+  };
+
 
   return (
-        <Container fluid>
-            
-            <h1>
-                {strings.registered_families[language]}
-            </h1>
+    <Container fluid>
+      <h1>{strings.registered_families[language]}</h1>
 
-            <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell align="right">{strings.first_name[language]}</TableCell>
-                    <TableCell align="right">{strings.last_name[language]}</TableCell>
-                    <TableCell align="right">{strings.city[language]}</TableCell>
-                    <TableCell align="right">{strings.street[language]}</TableCell>
-                    <TableCell align="right">{strings.house_number[language]}</TableCell>
-                    <TableCell align="right">{strings.apartment_number[language]}</TableCell>
-                    <TableCell align="right">{strings.phone_number[language]}</TableCell>
-                    <TableCell align="right">{strings.email[language]}</TableCell>
-                    <TableCell align="right">{strings.special_comment[language]}</TableCell>
-                    <TableCell align="right">{strings.confirmed[language]}</TableCell>
-                    <TableCell align="right">{strings.edit[language]}</TableCell>
-                    
-
-                    <TableCell ></TableCell>
-
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {families.map((family) => (
-                    <TableRow
-                    key={family.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell align="right">{family.first_name}</TableCell>
-                    <TableCell align="right">{family.last_name}</TableCell>
-                    <TableCell align="right">{family.city}</TableCell>
-                    <TableCell align="right">{family.street}</TableCell>
-                    <TableCell align="right">{family.house_number}</TableCell>
-                    <TableCell align="right">{family.apartment_number}</TableCell>
-                    <TableCell align="right">{family.phone_number}</TableCell>
-                    <TableCell align="right">{family.email}</TableCell>
-                    <TableCell align="right">{family.special_comment}</TableCell>
-                    <TableCell align="right">{family.confirmed ?<span>&#10004;</span> : ''}</TableCell>
-                    <TableCell align="right">
-                        <Button as={Link} to={`${family.id}`} variant="primary" state={{ family ,pathSuffix}} >{strings.edit[language]}</Button>
-                        </TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
-        </Container>
-    );
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">{strings.first_name[language]}</TableCell>
+              <TableCell align="right">{strings.last_name[language]}</TableCell>
+              <TableCell align="right">{strings.city[language]}</TableCell>
+              <TableCell align="right">{strings.street[language]}</TableCell>
+              <TableCell align="right">{strings.house_number[language]}</TableCell>
+              <TableCell align="right">{strings.apartment_number[language]}</TableCell>
+              <TableCell align="right">{strings.phone_number[language]}</TableCell>
+              <TableCell align="right">{strings.email[language]}</TableCell>
+              <TableCell align="right">{strings.special_comment[language]}</TableCell>
+              <TableCell align="right">{strings.confirmed[language]}</TableCell>
+              <TableCell align="right">{strings.edit[language]}</TableCell>
+              <TableCell align="right">{strings.assigning[language]}</TableCell>
+              <TableCell align="right">{strings.curAssigning[language]}</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {families.map((family, index) => (
+              <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell align="right">{family.first_name}</TableCell>
+                <TableCell align="right">{family.last_name}</TableCell>
+                <TableCell align="right">{family.city}</TableCell>
+                <TableCell align="right">{family.street}</TableCell>
+                <TableCell align="right">{family.house_number}</TableCell>
+                <TableCell align="right">{family.apartment_number}</TableCell>
+                <TableCell align="right">{family.phone_number}</TableCell>
+                <TableCell align="right">{family.email}</TableCell>
+                <TableCell align="right">{family.special_comment}</TableCell>
+                <TableCell align="right">
+                  <BsCheckCircleFill
+                    style={{ cursor: 'pointer' }}
+                    size={30}
+                    color={family.confirmed ? 'green' : 'grey'}
+                    onClick={() => confirm(index, !family.confirmed)}
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <BiPencil
+                    size={24}
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => {
+                      editClick(family);
+                    }}
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  {family.confirmed && (
+                    <FaChild
+                      size={24}
+                      color="#313B72"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleAssigningModal(family)}
+                    />
+                  )}
+                  <Assigning show={showModal} onHide={() => setShowModal(false)} language={language} event={event} selectedFamily={selectedFamily} setFamilies={setFamilies} settlement={settlement}/>
+                </TableCell>
+                <TableCell align="right">{family.camper}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
+
